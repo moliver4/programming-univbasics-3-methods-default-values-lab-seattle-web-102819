8e1a8ffc4ef650465f7ca73c120af8d9e4c85043
Tiffany Tang <tifftang22@gmail.com>
2019-10-01 21:07:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice(veg 1, veg2, protein = "meat")
+def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutrious meal!"
   string = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts string
